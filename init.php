f69e68dd666ceb8d9b6096effe2987209e8d8859
Joshua Byington <jbyington@paradowski.com>
2015-11-06 13:55:43 -0600

    composer.json updated, init updated, license file added

--- init.php
+++ init.php
@@ -5,22 +5,24 @@ use \puffin\app as app;
 
 ############################################
 
-define('SERVER_ROOT', dirname($_SERVER['DOCUMENT_ROOT']));
+<?php
 
-define('APP_PATH', SERVER_ROOT.'/app');
-define('CONTROLLER_PATH', APP_PATH.'/controllers');
-define('MODEL_PATH', APP_PATH.'/models');
-define('PLUGIN_PATH', APP_PATH.'/plugins');
-define('TEST_PATH', APP_PATH.'/tests');
-define('VIEW_PATH', APP_PATH.'/views');
-define('PARTIAL_PATH', VIEW_PATH.'/partials');
-define('LAYOUT_PATH', VIEW_PATH.'/layouts');
-define('SCRIPT_PATH', VIEW_PATH.'/scripts');
-define('PUBLIC_PATH', SERVER_ROOT.'/public');
-define('VENDOR_PATH', SERVER_ROOT.'/vendor');
-define('SYSTEM_PATH', VENDOR_PATH.'/puffin');
+if( !defined(SERVER_ROOT) ){ define('SERVER_ROOT', dirname($_SERVER['DOCUMENT_ROOT'])); }
+if( !defined(APP_PATH) ){ define('APP_PATH', SERVER_ROOT.'/app'); }
+if( !defined(VENDOR_PATH) ){ define('VENDOR_PATH', SERVER_ROOT.'/vendor'); }
+if( !defined(CONTROLLER_PATH) ){ define('CONTROLLER_PATH', APP_PATH.'/controllers'); }
+if( !defined(MODEL_PATH) ){ define('MODEL_PATH', APP_PATH.'/models'); }
+if( !defined(PLUGIN_PATH) ){ define('PLUGIN_PATH', APP_PATH.'/plugins'); }
+if( !defined(VIEW_PATH) ){ define('VIEW_PATH', APP_PATH.'/views'); }
+if( !defined(PARTIAL_PATH) ){ define('PARTIAL_PATH', VIEW_PATH.'/partials'); }
+if( !defined(LAYOUT_PATH) ){ define('LAYOUT_PATH', VIEW_PATH.'/layouts'); }
+if( !defined(SCRIPT_PATH) ){ define('SCRIPT_PATH', VIEW_PATH.'/scripts'); }
+if( !defined(PUBLIC_PATH) ){ define('PUBLIC_PATH', SERVER_ROOT.'/public'); }
+if( !defined(VENDOR_PATH) ){ define('VENDOR_PATH', SERVER_ROOT.'/vendor'); }
+if( !defined(SYSTEM_PATH) ){ define('SYSTEM_PATH', VENDOR_PATH.'/puffin'); }
 
-define('MUSTACHE_EXT', '.html');
+if( !defined(MUSTACHE_EXT) ){ define('MUSTACHE_EXT', '.html'); }
+
 
 ############################################
 
