7e526ea7143d055341cc4901ced999482574b562
Joshua Byington <jbyington@paradowski.com>
2016-02-13 00:41:57 -0600

    added error handling code to the app::route

--- src/app.php
+++ src/app.php
@@ -26,8 +26,30 @@ class app
 
     public static function route()
     {
-        $dispatcher = new Dispatcher( self::router()->getData() );
-        $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+		try
+		{
+			$dispatcher = new Dispatcher( self::router()->getData() );
+	        $results = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+		}
+		catch( \Exception $e )
+		{
+			switch( get_class($e) )
+			{
+				case 'Phroute\Phroute\Exception\HttpRouteNotFoundException':
+					#debug('HttpRouteNotFoundException');
+					http_response_code(404);
+					break;
+				case 'Phroute\Phroute\Exception\HttpMethodNotAllowedException':
+					#debug('HttpMethodNotAllowedException');
+					http_response_code(403);
+					break;
+				default:
+					#debug($e);
+					http_response_code(400);
+					break;
+			}
+		}
+
     }
 
     public static function render()
