45089695e94644b79d89ce0f7df6b79aae6d1fca
Joshua Byington <jbyington@paradowski.com>
2017-03-24 11:04:42 -0500

    Discovered bug in params. Possibly related to PHP7.

--- src/controller/param.php
+++ src/controller/param.php
@@ -50,10 +50,12 @@ class param
 		$output = '';
 		if( is_array($input) )
 		{
+			$output = [];
 			foreach( $input as $var => $val )
 			{
 				$output[$var] = $this->sanitize($val);
 			}
+			return $output;
 		}
 		else
 		{
@@ -63,19 +65,19 @@ class param
 			}
 			$output  = $this->cleanInput($input);
 		}
+
 		return $output;
 	}
 
 	protected function cleanInput($input)
 	{
 		$output = '';
-		$search = array
-		(
+		$search = [
 			'/<script[^>]*?>.*?<\/script>/si',   // Strip out javascript
 			'/<[\/\!]*?[^<>]*?>/si',            // Strip out HTML tags
 			'/<style[^>]*?>.*?<\/style>/siU',    // Strip style tags properly
 			'/<![\s\S]*?--[ \t\n\r]*>/'         // Strip multi-line comments
-		);
+		];
 
 		$output = preg_replace($search, '', $input);
 		return $output;
